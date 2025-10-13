3a9b049ef42d730babdf084aacfdf404039b4cd1
BPLboi <boyanlitchev@yahoo.com>
2025-10-13 11:34:10 -0400

    finished API queries

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -15,6 +16,9 @@ import java.util.*;
  * exceptions to align with the requirements of the BreedFetcher interface.
  */
 public class DogApiBreedFetcher implements BreedFetcher {
+    private static final String STATUS_CODE = "status";
+    private static final String SUCCESS_CODE = "success";
+    private static final String SUBBREEDS_CODE = "message";
     private final OkHttpClient client = new OkHttpClient();
 
     /**
@@ -30,6 +34,29 @@ public class DogApiBreedFetcher implements BreedFetcher {
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        final String API_URL = "https://dog.ceo/api/breed/" + breed + "/list";
+
+
+        final Request request = new Request.Builder().url(API_URL).build();
+
+        try{
+            final Response response = client.newCall(request).execute();
+            final JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getString(STATUS_CODE).equals(SUCCESS_CODE)) {
+                final JSONArray subbreeds = responseBody.getJSONArray(SUBBREEDS_CODE);
+                final List<String> subreeds_list = new ArrayList<>();
+                for (int i = 0; i < subbreeds.length(); i++) {
+                    subreeds_list.add(subbreeds.getString(i));
+                }
+                return subreeds_list;
+            }
+            else {
+                throw new RuntimeException("Unsuccessful query to DogBreedApi");
+            }
+        }
+        catch (IOException | JSONException event){
+            throw new RuntimeException(event);
+        }
     }
 }
